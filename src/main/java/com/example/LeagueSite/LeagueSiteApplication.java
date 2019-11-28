94f231e309f24c8d78bfebb7191028c36fa499e7
Andre Lussier <Dremon7@hotmail.com>
2019-11-28 13:18:09 -0500

    initial commit

---
+++ src/main/java/com/example/LeagueSite/LeagueSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeagueSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeagueSite/LeagueSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeagueSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
